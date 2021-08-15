86fa1fda73c3798463046ebe237b1108c5dd219a
AKASHGUPTA <akashgupta77500@gmail.com>
2021-08-15 14:16:38 +0530

    testing mode

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vpmtr7w0h!@ey(5w7au)!x1if0+mr+twfh#h05oqjp)try-5$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'jobmilegaonline.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'jobmilega.online']
 
 
 # Application definition
